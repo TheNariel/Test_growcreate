ca3ccc6fdc1e7dd1bfbbf65888de32ee1bf1a28f
Tomáš Voznička <voznickatomas@gmail.com>
2018-02-11 18:56:22 +0000

    Geting the lat and lgn  of address

--- Test_growcreate/Controllers/HomeController.cs
+++ Test_growcreate/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,18 +17,22 @@ namespace Test_growcreate.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        [HttpPost]
+        public ActionResult Index(String address)
+        { 
+            string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/json?address={0}&key=AIzaSyBIeDWTn7QhIeRzokoz7VndonQANW4-Wdg", Uri.EscapeDataString(address));
 
-            return View();
-        }
+            WebRequest request = WebRequest.Create(requestUri);
+            WebResponse response = request.GetResponse();
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd();
+            JObject o = JObject.Parse(json);
+            ViewBag.Lat=(string)o["results"][0]["geometry"]["location"]["lat"];
+            ViewBag.Lng=(string)o["results"][0]["geometry"]["location"]["lng"];
+            
             return View();
         }
     }
+   
 }
